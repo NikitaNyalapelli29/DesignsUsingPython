dbb425ad27546593df1f9ad81ffe35d90683d533
Nikita Nyalapelli <85953042+NikitaNyalapelli29@users.noreply.github.com>
2022-05-09 22:26:05 +0530

    Create Change the Pen Direction.py

---
+++ Change the Pen Direction.py
@@ -0,0 +1,16 @@
+import turtle  
+t = turtle.Turtle()  
+  
+t.fd(100)  
+t.rt(90)  
+t.penup()  
+t.fd(100)  
+t.rt(90)  
+t.pendown()  
+t.fd(100)  
+t.rt(90)  
+t.penup()  
+t.fd(100)  
+t.pendown()  
+  
+turtle.mainloop()  
